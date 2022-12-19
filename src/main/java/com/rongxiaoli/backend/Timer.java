9d838c890f4dd2dffc9123deecb9ba4e99c680e3
Rong_Xiaoli <1751362263@qq.com>
2022-12-20 01:36:54 +0800

    0.1.4 DailySign --WIP: Data.java

--- src/main/java/com/rongxiaoli/backend/Timer.java
+++ src/main/java/com/rongxiaoli/backend/Timer.java
@@ -2,20 +2,20 @@ package com.rongxiaoli.backend;
 
 import org.jetbrains.annotations.Nullable;
 
-public abstract class TimerExecute {
+public abstract class Timer {
     public Object Args;
     public boolean Running = false;
     public String PluginName;
     public abstract void run(@Nullable Object args);
-    public TimerExecute(String PluginName) {
+    public Timer(String PluginName) {
         this.PluginName = PluginName;
     }
 
-    public final class Timer extends Thread {
+    public class Executer extends Thread {
         public void run() {
             Running = true;
             while (Running) {
-                TimerExecute.this.run(Args);
+                com.rongxiaoli.backend.Timer.this.run(Args);
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
--- src/main/java/com/rongxiaoli/module/DailySign/DailySign.java
+++ src/main/java/com/rongxiaoli/module/DailySign/DailySign.java
@@ -1,114 +1,54 @@
 package com.rongxiaoli.module.DailySign;
 
 import com.rongxiaoli.Module;
-import com.rongxiaoli.RongXiaoliBot;
-import com.rongxiaoli.backend.JSONFile;
-import com.rongxiaoli.backend.Log;
-import com.rongxiaoli.backend.TimerExecute;
-import com.rongxiaoli.module.DailySign.ModuleBackend.SignIn.Data;
-import com.rongxiaoli.module.DailySign.ModuleBackend.SignIn.SignInRequestProcess;
-import com.rongxiaoli.module.DailySign.ModuleBackend.SignIn.SignObjectList;
+import com.rongxiaoli.backend.Timer;
 import net.mamoe.mirai.contact.Contact;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.Objects;
-
 public class DailySign extends Module {
-    private static final String CommandPrefix = "Rsign";
-    private static final String HelpContent = "Rsign\n" +
-            "签到";
-    public static Data SignList = new Data();
-    public static DateChecker DChecker = new DateChecker();
-    private static JSONFile SignInDataJSONFile;
-    private static String PluginName = "DailySign";
-    private boolean IsEnabled = false;
+    // Public vars.
+    private String PluginName = "DailySign";
+    private String HelpContent;
+    private boolean IsEnabled = true;
+    private boolean DebugMode = false;
+    // Private vars.
 
+    // Vars def end.
     /**
-     * Module init func.
+     * Module initiate function.
      */
     public void Init() {
-        SignInDataJSONFile = new JSONFile(PluginName, RongXiaoliBot.DataPath.toString(), "/DailySign/SignData.json");
-        SignInDataJSONFile.JObject = new Data();
-        SignInDataJSONFile.readFile();
-        SignInDataJSONFile.JObject = SignInDataJSONFile.toObject();
-        DailySign.SignList = (Data) SignInDataJSONFile.JObject;
 
-        Log.WriteLog(Log.Level.Verbose,
-                "DailySign JSON file readed. ",
-                Log.LogClass.ModuleMain,
-                PluginName);
-
-        DChecker.run(null);
-        Log.WriteLog(Log.Level.Verbose,
-                "DailySign DateChecker ready. ",
-                Log.LogClass.ModuleMain,
-                PluginName);
-
-        if (DailySign.SignList == null) {
-            Log.WriteLog(Log.Level.Warning,
-                    "SignList is null. ",
-                    Log.LogClass.ModuleMain,
-                    DailySign.PluginName);
-            SignList = new Data();
-        }
-        Log.WriteLog(Log.Level.Debug,
-                "DailySign plugin initiated. ",
-                Log.LogClass.ModuleMain,
-                PluginName);
-        IsEnabled = true;
     }
 
     /**
-     * Module shutdown func.
+     * Module shutdown function.
      */
     public void Shutdown() {
-        SignInDataJSONFile.JString = null;
-        SignInDataJSONFile.JObject = SignList;
-        SignInDataJSONFile.JString = new StringBuilder();
-        SignInDataJSONFile.JString.append(SignInDataJSONFile);
-        SignInDataJSONFile.writeFile();
-        Log.WriteLog(Log.Level.Debug,
-                "DailySign JSONData saved! ",
-                Log.LogClass.ModuleMain,
-                PluginName);
+
     }
 
+    /**
+     * Friend message process.
+     *
+     * @param arrCommand
+     * @param Friend
+     * @param SenderContact
+     */
     public void FriendMain(String[] arrCommand, long Friend, Contact SenderContact) {
-        //Judge if command is 0-width.
-        if (arrCommand.length == 0) {
-            return;
-        }
-        //Judge if prefix is correct.
-        if (!Objects.equals(arrCommand[0], CommandPrefix)) {
-            return;
-        }
-        //Judge if the plugin is enabled.
-        if (!IsEnabled) {
-            SenderContact.sendMessage("当前插件未启用");
-            return;
-        }
-        //Start processing.
-        SignInRequestProcess.Process(arrCommand, Friend, 0, SenderContact);
+
     }
 
+    /**
+     * Group message process.
+     *
+     * @param arrCommand
+     * @param Friend
+     * @param Group
+     * @param SenderContact
+     */
     public void GroupMain(String[] arrCommand, long Friend, long Group, Contact SenderContact) {
-        //Judge if command is 0-width.
-        if (arrCommand.length == 0) {
-            return;
-        }
-        //Judge if prefix is correct.
-        if (!Objects.equals(arrCommand[0], CommandPrefix)) {
-            return;
-        }
-        //Judge if the plugin is enabled.
-        if (!IsEnabled) {
-            SenderContact.sendMessage("当前插件未启用");
-            return;
-        }
-        //Start processing.
-        SignInRequestProcess.Process(arrCommand, Friend, Group, SenderContact);
+
     }
 
     /**
@@ -129,7 +69,7 @@ public class DailySign extends Module {
      * True if enabled.
      */
     public boolean isEnabled() {
-        return IsEnabled;
+        return this.IsEnabled;
     }
 
     /**
@@ -138,43 +78,13 @@ public class DailySign extends Module {
      * @param status Status
      */
     public void setEnabled(boolean status) {
-        IsEnabled = status;
+        this.IsEnabled = isEnabled();
     }
 
     /**
      * Debug mode.
      */
     public boolean isDebugMode() {
-        return false;
-    }
-
-    public static class DateChecker extends TimerExecute {
-        public static int Month;
-        public static int Day;
-
-        public DateChecker() {
-            super(DailySign.PluginName);
-        }
-
-        @Override
-        public void run(@Nullable Object args) {
-            GregorianCalendar GC = new GregorianCalendar();
-            if (Month != GC.get(Calendar.MONTH) || Day != GC.get(Calendar.DAY_OF_MONTH)) {
-                if (SignList == null) {
-                    Month = GC.get(Calendar.MONTH);
-                    Day = GC.get(Calendar.DAY_OF_MONTH);
-                    return;
-                }
-                for (SignObjectList.GroupSignObject SingleObject :
-                        SignList.GroupSignList) {
-                    SingleObject.Position = 1;
-                }
-                Month = GC.get(Calendar.MONTH);
-                Day = GC.get(Calendar.DAY_OF_MONTH);
-            }
-            SignInDataJSONFile.JObject = SignList;
-            SignInDataJSONFile = new JSONFile(PluginName, RongXiaoliBot.DataPath.toString(), "/DailySign/SignData.json");
-            SignInDataJSONFile.writeFile();
-        }
+        return DebugMode;
     }
 }
