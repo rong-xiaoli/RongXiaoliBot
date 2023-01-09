c96cc441f272e5c68db6fb667c4542330cffd566
Rong_Xiaoli <1751362263@qq.com>
2023-01-10 00:27:45 +0800

    0.1.5 Debug --Added FileHelper (WIP). --Reedited welcome word in AutoAcceptPlugin.Main. --Considering using JSON in BannedWordCheck.

--- src/main/java/com/rongxiaoli/module/AutoAccept/AutoAcceptPlugin.java
+++ src/main/java/com/rongxiaoli/module/AutoAccept/AutoAcceptPlugin.java
@@ -44,9 +44,10 @@ public class AutoAcceptPlugin extends Module {
         }
         MessageChainBuilder WelcomeMessage = new MessageChainBuilder();
         WelcomeMessage.append("欢迎使用容小狸Bot！\n");
-        WelcomeMessage.append("目前主要功能：\n");
-        WelcomeMessage.append("涩图，命令：setu [Keyword1] [keyword2] ...\n");
-        WelcomeMessage.append("打卡，命令：sign");
+        WelcomeMessage.append("项目地址：https://github.com/rong-xiaoli/RongXiaoliBot\n");
+        WelcomeMessage.append("请输入/help以查看命令帮助。\n");
+        WelcomeMessage.append("请访问项目地址以查看最新功能。");
+
         e.getBot().getFriend(e.getFriend().getId()).sendMessage(WelcomeMessage.build());
     }
 
--- src/main/java/com/rongxiaoli/module/Repeater/backend/BannedWordCheck.java
+++ src/main/java/com/rongxiaoli/module/Repeater/backend/BannedWordCheck.java
@@ -1,8 +1,17 @@
 package com.rongxiaoli.module.Repeater.backend;
 
+import com.rongxiaoli.backend.StringProcess;
+
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class need to check the repeating message is legal or not.
+ * Consider using JSON file. Is it overkilled?
+ * JSON is the simplest solution now until I finish file operation.
+ * If someone is interested in it (no there will be not), you can refactor it.
+ */
 public class BannedWordCheck {
     private String configFile;
     /**
@@ -13,10 +22,30 @@ public class BannedWordCheck {
         configFile = conPath + "BannedWord.txt";
         BannedWordList = new ArrayList<>();
         //Todo: Finish it.
-
+        //Load word list.
+        File bannedWordFile = new File(configFile);
     }
     public boolean isSuitable(String[] originalMessage) {
-        //Todo: Finish it.
-        return false;
+        // Start processing.
+        // First step: check raw string.
+        for(String singleString :
+                originalMessage) {
+            if (BannedWordList.contains(originalMessage)) {
+                return false;
+            }
+        }
+
+        // Second step: remove empty string and spaces.
+        String[] ImpString = StringProcess.Imperative(originalMessage);
+        for(String singleString :
+                ImpString) {
+            if (BannedWordList.contains(singleString)) {
+                return false;
+            }
+        }
+
+        // Finish.
+        return true;
+
     }
 }
